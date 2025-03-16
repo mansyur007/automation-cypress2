08b1a1dfa7b206247ebc992039887548280f8bd5
mansyur007 <mansyurhidayat007@gmail.com>
2025-03-16 23:45:21 +0700

    save video and add waiting

--- cypress.config.js
+++ cypress.config.js
@@ -12,6 +12,8 @@ module.exports = defineConfig({
     experimentalRunAllSpecs: true,
     viewportHeight: 1400,
     viewportWidth: 1600,
+    video: true,
+    videoCompression: false,
   },
 });
 
